aa4a9e90ad46e172ed6bbcc1cc04dc19d64a1f68
Ben Hochstedler <10050028+UltimateGeek@users.noreply.github.com>
2020-12-29 20:08:42 -0500

    Establish build script for src files to vtt files

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ To extract the files to view their source:
     ./util/extract.sh
 
 That creates a src dir with all the *.vtt files expanded.
+
+To recreate the vtt files from source:
+
+    ./util/build.sh
+
+Note: this may cause vtt files to appear changed due to file timestamps
+and relative vs absolute pathing of the zipped files.
---
+++ util/build.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+utildir=$(dirname "$0")
+
+SRC_DIR=src
+
+function usage() {
+  exit_code=$1
+
+  script_name=$(basename "$0")
+  cat <<EOF
+Usage:$script_name
+
+Builds the files in $SRC_DIR dir into vtt files
+EOF
+  exit "$exit_code"
+}
+
+cd "$utildir" || (echo "Unable to traverse project." && usage 2)
+while ! test -f package.json; do
+  cd .. || (echo "Could not find root of project." && usage 3)
+done
+
+projdir=$(pwd)
+
+failed=""
+
+cd "$SRC_DIR"
+
+for f in */*.json */*/*.json; do
+  subdir=$(dirname "$f")
+  vtt_file="$projdir/${subdir}.vtt"
+
+  #todo: filter out duplicates to prevent needlessly re-zip ones with multiple json files
+
+  echo "$vtt_file <= $subdir"
+  (rm -f "$vtt_file" && cd "$subdir" && zip "$vtt_file" -r *) || failed="$failed $file"
+  echo ""
+done
+
+if [[ ! -z "$failed" ]]; then
+  echo ""
+  echo "Failures encountered extacting the following:"
+  echo "$failed"
+  exit 1
+fi
